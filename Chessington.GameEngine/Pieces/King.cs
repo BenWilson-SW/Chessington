92f51ebbf32e3bc3931acbba5cec42d23d11bde9
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-10 16:25:58 +0100

    Add basic king movement

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -5,12 +5,32 @@ namespace Chessington.GameEngine.Pieces
 {
     public class King : Piece
     {
+        private readonly (int, int)[] kingMoveOffsets =
+        [
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0),
+            (1, 1),
+            (-1, -1),
+            (1, -1),
+            (-1, 1)
+        ];
+        
         public King(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var king = board.FindPiece(this);
+
+            foreach (var (row, col) in kingMoveOffsets)
+            {
+                moves.Add(Square.At(king.Row + row, king.Col + col));
+            }
+            
+            return moves;
         }
     }
 }
