da85bfe47b536e44da4904edc6c45d952a1b1536
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-10 15:25:07 +0100

    Add basic bishop movement

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,30 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var bishop = board.FindPiece(this);
+            
+            for (int i = 1; i <= Math.Min(7 - bishop.Row, 7 - bishop.Col); i++)
+            {
+                moves.Add(Square.At(bishop.Row + i, bishop.Col + i));
+            }
+            
+            for (int i = 1; i <= Math.Min(7 - bishop.Row, bishop.Col); i++)
+            {
+                moves.Add(Square.At(bishop.Row + i, bishop.Col - i));
+            }
+            
+            for (int i = 1; i <= Math.Min(bishop.Row, bishop.Col); i++)
+            {
+                moves.Add(Square.At(bishop.Row - i, bishop.Col - i));
+            }
+            
+            for (int i = 1; i <= Math.Min(bishop.Row, 7 - bishop.Col); i++)
+            {
+                moves.Add(Square.At(bishop.Row - i, bishop.Col + i));
+            }
+
+            return moves;
         }
     }
 }
