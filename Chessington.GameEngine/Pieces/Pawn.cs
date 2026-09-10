c1c3144cbc7f829f6e4b7b148923b327c0c4fa6f
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-10 16:34:29 +0100

    Stop pawns moving through pieces

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -15,14 +15,29 @@ namespace Chessington.GameEngine.Pieces
 
             var direction = Player == Player.White ? -1 : 1;
             var startRow = Player == Player.White ? 6 : 1;
+
+            var move = Square.At(pawn.Row + direction, pawn.Col);
             
-            moves.Add(Square.At(pawn.Row + direction, pawn.Col));
-
-            if (pawn.Row == startRow)
+            if (board.GetPiece(move) != null)
             {
-                moves.Add(Square.At(pawn.Row + 2 * direction, pawn.Col));                
+                return moves;
             }
+            
+            moves.Add(move);
 
+            if (pawn.Row != startRow)
+            {
+                return moves;
+            }
+            
+            move = Square.At(pawn.Row + 2 * direction, pawn.Col);
+            
+            if (board.GetPiece(move) != null)
+            {
+                return moves;
+            }
+            
+            moves.Add(move);
             return moves;
         }
     }
