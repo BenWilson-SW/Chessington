71f0f58869262cb6c10cce49bc55fea6d6113dd2
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-10 16:10:15 +0100

    Add basic knight moves

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -5,12 +5,32 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Knight : Piece
     {
+        private readonly (int, int)[] knightMoveOffsets =
+        [
+            (1, 2),
+            (2, 1),
+            (-1, 2),
+            (-2, 1),
+            (1, -2),
+            (2, -1),
+            (-1, -2),
+            (-2, -1)
+        ];
+        
         public Knight(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var knight = board.FindPiece(this);
+            
+            foreach (var (rows, cols) in knightMoveOffsets)
+            {
+                moves.Add(Square.At(knight.Row + rows, knight.Col + cols));
+            }
+            
+            return moves;
         }
     }
 }
