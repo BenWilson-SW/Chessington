b96605663b258890c59513e322b488d858af6591
Ben Wilson <Ben.Wilson@softwire.com>
2026-09-15 09:59:34 +0100

    Refactor sliding move generation in PieceHelper
    This places all of the duplicated code in loops into a single function, but I think the code is a bit harder to parse than before.
    
    I am also unsure whether it is best to keep these in a "PieceHelper" class or not. For example these could be moved to the Rook and Bishop classes. They would still need the common ValidSlidingMoves code though, and the Queen class would then depend on Rook and Bishop, which seems not to be "proper".

--- Chessington.GameEngine/Pieces/PieceHelper.cs
+++ Chessington.GameEngine/Pieces/PieceHelper.cs
@@ -1,81 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chessington.GameEngine.Pieces;
 
 public class PieceHelper
 {
+    private static IEnumerable<Square> ValidSlidingMoves(Player player, Board board, IEnumerable<Square> allMoves)
+    {
+        foreach (var move in allMoves)
+        {
+            var piece = board.GetPiece(move);
+
+            if (piece == null)
+            {
+                yield return move;
+            }
+            else
+            {
+                if (piece.Player != player)
+                {
+                    yield return move;
+                }
+                
+                yield break;
+            }
+        }
+    }
+    
     public static IEnumerable<Square> GetAvailableLateralMoves(Player player, Board board, Square from)
     {
         var moves = new List<Square>();
 
-        for (var col = from.Col - 1; col >= 0; col--)
-        {
-            var move = Square.At(from.Row, col);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-
-                break;
-            }
-            
-            moves.Add(move);
-        }
-            
-        for (var col = from.Col + 1; col < 8; col++)
-        {
-            var move = Square.At(from.Row, col);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-                
-                break;
-            }
-            
-            moves.Add(move);
-        }
-            
-        for (var row = from.Row - 1; row >= 0; row--)
-        {
-            var move = Square.At(row, from.Col);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-                
-                break;
-            }
-            
-            moves.Add(move);
-        }
-            
-        for (var row = from.Row + 1; row < 8; row++)
-        {
-            var move = Square.At(row, from.Col);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-                
-                break;
-            }
-            
-            moves.Add(move);
-        }
+        moves.AddRange(ValidSlidingMoves(
+                player, board,
+                Enumerable.Range(0, from.Col)
+                    .Reverse()
+                    .Select(col => Square.At(from.Row, col))
+            ));
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(from.Col + 1, 7 - from.Col)
+                .Select(col => Square.At(from.Row, col))
+        ));
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(0, from.Row)
+                .Reverse()
+                .Select(row => Square.At(row, from.Col))
+        ));
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(from.Row + 1, 7 - from.Row)
+                .Select(row => Square.At(row, from.Col))
+        ));
 
         return moves;        
     }
@@ -83,74 +64,30 @@ public class PieceHelper
     public static IEnumerable<Square> GetAvailableDiagonalMoves(Player player, Board board, Square from)
     {
         var moves = new List<Square>();
-            
-        for (int i = 1; i <= Math.Min(7 - from.Row, 7 - from.Col); i++)
-        {
-            var move = Square.At(from.Row + i, from.Col + i);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-                
-                break;
-            }
-            
-            moves.Add(move);
-        }
-            
-        for (int i = 1; i <= Math.Min(7 - from.Row, from.Col); i++)
-        {
-            var move = Square.At(from.Row + i, from.Col - i);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-                
-                break;
-            }
-            
-            moves.Add(move);
-        }
-            
-        for (int i = 1; i <= Math.Min(from.Row, from.Col); i++)
-        {
-            var move = Square.At(from.Row - i, from.Col - i);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-
-                break;
-            }
-            
-            moves.Add(move);
-        }
-            
-        for (int i = 1; i <= Math.Min(from.Row, 7 - from.Col); i++)
-        {
-            var move = Square.At(from.Row - i, from.Col + i);
-            var piece = board.GetPiece(move);
-            if (piece != null)
-            {
-                if (piece.Player != player)
-                {
-                    moves.Add(move);                    
-                }
-
-                break;
-            }
-            
-            moves.Add(move);
-        }
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(1, Math.Min(7 - from.Row, 7 - from.Col))
+                .Select(i => Square.At(from.Row + i, from.Col + i))
+        ));
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(1, Math.Min(7 - from.Row, from.Col))
+                .Select(i => Square.At(from.Row + i, from.Col - i))
+        ));
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(1, Math.Min(from.Row, from.Col))
+                .Select(i => Square.At(from.Row - i, from.Col - i))
+        ));
+        
+        moves.AddRange(ValidSlidingMoves(
+            player, board,
+            Enumerable.Range(1, Math.Min(from.Row, 7 - from.Col))
+                .Select(i => Square.At(from.Row - i, from.Col + i))
+        ));
 
         return moves;
     }
